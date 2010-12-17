740de645a77ca79f24aab011c9156010f96db1aa
Tim Becker <tim.becker@kuriositaet.de>
2010-12-17 14:28:43 +0100

    conflict

--- src/pkg/crypto/des/des_cbc_test.go
+++ src/pkg/crypto/des/des_cbc_test.go
@@ -3,6 +3,7 @@ package des
 import (
   "testing"
   "crypto/block"
+  "crypto/cipher"
   "bytes"
   "io"
 )
@@ -69,4 +70,45 @@ func TestCBC_DES (t *testing.T) {
   des (t, cbcDESTests[0])
   des2(t, cbcDESTests[1])
   des3(t, cbcDESTests[2])
+}
+
+
+// different tests
+
+var cbcDES3Tests = []struct {
+  key []byte
+  iv  []byte
+  in  []byte
+  out []byte
+}{
+  {
+    commonKey24,
+    zeroIV8,
+    []byte{1, 2, 3, 4, 5, 6, 7, 8},
+    []byte{0x2e, 0xe4, 0xfc, 0x93, 0x96, 0x7, 0x7e, 0xd},
+  },
+}
+
+func TestCBC_DES3(t *testing.T) {
+  for _, tt := range cbcDES3Tests {
+    c, err := NewDES3Cipher(tt.key)
+    if err != nil {
+      t.Errorf("NewDES3Cipher(%d bytes) = %s", len(tt.key), err)
+      continue
+    }
+
+    encrypter := cipher.NewCBCEncrypter(c, tt.iv)
+    d := make([]byte, len(tt.in))
+    encrypter.CryptBlocks(d, tt.in)
+    if !bytes.Equal(tt.out, d) {
+      t.Errorf("CBCEncrypter\nhave %x\nwant %x", d, tt.out)
+    }
+
+    decrypter := cipher.NewCBCDecrypter(c, tt.iv)
+    p := make([]byte, len(d))
+    decrypter.CryptBlocks(p, d)
+    if !bytes.Equal(tt.in, p) {
+      t.Errorf("CBCDecrypter\nhave %x\nwant %x", p, tt.in)
+    }
+  }
 }
